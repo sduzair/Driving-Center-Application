72410f3ac41324317cc668dd2025792b44497df4
sduzair <sduzairahmed@gmail.com>
2024-10-14 05:00:01 -0400

    ci(perf): deoptimize ci github workflow and remove parallel testing Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -14,13 +14,13 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 module.exports = defineConfig({
 	testDir: "./tests",
 	/* Run tests in files in parallel */
-	fullyParallel: true,
+	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* No retries */
 	retries: 0,
 	/* Opt out of parallel tests on CI. */
-	// workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
